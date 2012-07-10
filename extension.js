4f7735d919fa77e78bb1df181e9ae16d038b5be6
Soe <soe@soe.im>
2012-07-10 05:48:59 +0200

    v 0.3 - with more comments now

--- extension.js
+++ extension.js
@@ -4,11 +4,13 @@
   http://crossrider.wiki.zoho.com
 *************************************************************************************/
 
+// when crossrider app is ready
 appAPI.ready(function($) {
 
-	// don't do anything if not Google Drive page
+	// don't do anything if the page is not the GDrive page
 	if (!appAPI.isMatchPages("drive.google.com/*")) return;
-		
+	
+	// a central place to define various messages used in the app	
 	var MESSAGES = {
 		chatter_login: 'Please login to Salesforce first...',
 		chatter_loading: 'Loading...',
@@ -17,41 +19,46 @@ appAPI.ready(function($) {
 		endpoint_sample: 'https://c.na9.visual.force.com/apex/gdrive_chatter',
 	};
 	
-	var docTitle, iframeTimer;
-
-	// load local resources
+	// load stylesheet from the local resources
 	appAPI.resources.includeCSS('css/extension.css');
 	
-	// chatter endpoint button
-	$('<div class="goog-menuseparator navpane-download-separator" role="separator"></div><a id="chatter-endpoint-btn" class="download-link"><span class="goog-tree-item-label">'+ MESSAGES['endpoint_btn_text'] +'</span></a>').appendTo('#navpane-container-scrollable');
+	// chatter endpoint button - which is inserted on the GDrive page's left column
+	$('<div class="goog-menuseparator navpane-download-separator" role="separator"></div>'+ 
+		'<a id="chatter-endpoint-btn" class="download-link">'+ 
+		'<span class="goog-tree-item-label">'+ MESSAGES['endpoint_btn_text'] +'</span>'+
+		'</a>'
+	).appendTo('#navpane-container-scrollable');
 	
-	// insert chatter icons
+	// .chatter-btn (chatter button) - insert them for each file
 	$('.doclist-td-name').each(function() {
 		$(this).prepend('<div class="chatter-btn">&nbsp;</div>');
 	});
 	
-	// insert modal
+	// insert modal box inside the document body
 	$('<div id="modal-wrapper" style="display: none;"><div id="modal" style="width: 580px; height: 440px;" /></div>').appendTo('body');
 	
-	// insert close button
+	// insert close button inside the modal box
 	$('<div id="modal-title"><span id="modal-title-text">Title Here</span><span id="modal-close-btn">x</span><div style="clear: both" /></div>').appendTo('#modal');
 	
-	// insert modal-body
+	// insert modal body inside the modal box
 	$('<div id="modal-body"><iframe src="" marginwidth=0 marginheight=0 frameborder=0 scrolling=yes style="border: 0; width: 560px; height: 360px; margin: 10px 8px 10px 12px;" /></div>').appendTo('#modal');	
 	
-	// press escape - hide modal
-	$(document).keyup(function(e) {
+	// on escape key pressed - hide modal box
+	$(document).on('keyup', function(e) {
   		if (e.keyCode == 27) $('#modal-wrapper').fadeOut('fast');  // esc
 	});
 	
-	// listen to click on #modal-close-btn - hide modal
+	// listen to click on #modal-close-btn
+	// when close button is clicked, hide the modal box
 	$('#modal-close-btn').on('click', function() {
 		$('#modal-wrapper').fadeOut('fast');
 	});
 	
-	// delegate to click on td.doclist-td-name - when chatter icon is clicked
+	// delegate to click on td.doclist-td-name
+	// when chatter icon is clicked, show the modal box and load the relevant content
 	$('body').delegate('td.doclist-td-name', 'click', function(e) {
 		
+		// check if the click is really on the .chatter-btn
 		if(!$(this).hasClass('doclist-td-name')) return;
 		
 		//console.log($(this).attr('id').split('.')[2]);
@@ -60,27 +67,32 @@ appAPI.ready(function($) {
 		// if chatter_endpoint is blank, prompt for entry
 		if(!appAPI.db.get('chatter_endpoint')) $('#chatter-endpoint-btn').click();
 	
-		// set iframe src
+		// set iframe src to the visualforce page with DocumentID appended
 		var _url = appAPI.db.get('chatter_endpoint') +'?DocumentID='+ $(this).attr('id').split('.')[2];
 		
-		
-		docTitle = $(this).find('.doclist-content-wrapper').html();
+		// document title - shown inside the modal box
+		var docTitle = $(this).find('.doclist-content-wrapper').html();
 		
 		// set loading message
 		$('#modal-title-text').html(MESSAGES['chatter_loading']);
 		
-		// show modal
+		// show the modal box
 		$('#modal-wrapper').fadeIn('fast');
 		
+		// an ajax call to check if the user is logged in
+		// salesforce doesn't allow the login page to be iframed
+		// so user is asked to login on his/her own
 		appAPI.request.get(_url, function(resp) {
-			if($(resp).find('#newfeed').length) { // iframe success
+
+			// if #newfeed element is there - it is not a login page
+			if($(resp).find('#newfeed').length) { // so user is logged in
 			
 				// change #modal-title-text to title
 				$('#modal-title-text').html(docTitle);
 				
 				// set iframe src
 				$('#modal-body > iframe').attr('src', _url);
-			} else { // iframe fail - ask user to login
+			} else { // user is not logged in - ask uthe ser to login
 			
 				// change #modal-title-text to login message 
 				$('#modal-title-text').html(MESSAGES['chatter_login']);
@@ -88,15 +100,17 @@ appAPI.ready(function($) {
 				// set iframe src to blank page
 				$('#modal-body > iframe').attr('src', '');
 			}
+
 		}, function(resp) {
 			
 		}); // end appAPI.request.get
 		
-		e.preventDefault();
+		e.preventDefault(); // stop the default event behavior
 	});
 
-	// listen to change - when google drive page is modified via JS
-	setInterval(function() { // a bit stupid but mutation events are not suppored well across browsers
+	// at defined interval insert the .chatter-btn (chatter button)
+	// needed to rely on the interval as GDrive page changes a lot via JS
+	setInterval(function() { 
 		$('.chatter-btn').remove();
 		$('.doclist-td-name').each(function() {
 			$(this).prepend('<div class="chatter-btn">&nbsp;</div>');
@@ -104,6 +118,8 @@ appAPI.ready(function($) {
 	}, 800);
 	
 	// listen to click #chatter-endpoint-btn
+	// when the button is clicked, show the prompt box where user can change the endpoint
+	// it is a nice feature, as user can switch between various salesforce accounts with just one plugin
 	$('#chatter-endpoint-btn').on('click', function(e) {
 		var _endpoint = prompt(MESSAGES['endpoint_prompt'], appAPI.db.get('chatter_endpoint'));
 		if(_endpoint) appAPI.db.set('chatter_endpoint', _endpoint);
@@ -111,7 +127,6 @@ appAPI.ready(function($) {
 		e.preventDefault();
 	});
 	
-	// if chatter_endpoint is blank, prompt for entry
-	// if(!appAPI.db.get('chatter_endpoint')) $('#chatter-endpoint-btn').click();
+	// if chatter_endpoint is blank, set is to the sample endpoint
 	if(!appAPI.db.get('chatter_endpoint')) appAPI.db.set('chatter_endpoint', MESSAGES['endpoint_sample']);
 });
